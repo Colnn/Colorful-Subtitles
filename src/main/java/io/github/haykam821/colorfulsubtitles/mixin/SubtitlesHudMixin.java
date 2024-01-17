d448158f6589a9909dfb53998358126d8ac84ea5
_Coln <csoer1947@student.landstede.nl>
2024-01-17 20:13:04 +0100

    Updated to 1.20.1

--- src/main/java/io/github/haykam821/colorfulsubtitles/mixin/SubtitlesHudMixin.java
+++ src/main/java/io/github/haykam821/colorfulsubtitles/mixin/SubtitlesHudMixin.java
@@ -3,6 +3,7 @@ package io.github.haykam821.colorfulsubtitles.mixin;
 import java.util.Iterator;
 import java.util.List;
 
+import net.minecraft.util.math.ColorHelper;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -33,9 +34,9 @@ public class SubtitlesHudMixin {
 		return this.iterationEntry = (ColorHolder) iterator.next();
 	}
 
-	@ModifyArg(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/SubtitlesHud;drawTextWithShadow(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/font/TextRenderer;Lnet/minecraft/text/Text;III)V"), index = 5)
+	@ModifyArg(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawTextWithShadow(Lnet/minecraft/client/font/TextRenderer;Lnet/minecraft/text/Text;III)I"), index = 4)
 	private int modifyDrawColor(int color) {
-		return MathHelper.multiplyColors(color, this.iterationEntry.getColor());
+		return ColorHelper.Argb.mixColor(color, this.iterationEntry.getColor());
 	}
 
 	@Inject(method = "onSoundPlayed", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/SubtitlesHud$SubtitleEntry;reset(Lnet/minecraft/util/math/Vec3d;)V"), locals = LocalCapture.CAPTURE_FAILHARD)
